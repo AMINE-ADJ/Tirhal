40d3ccc9ded3699934a7626fad085b406101fe86
Amine <ka_adjou@esi.dz>
2023-05-25 14:15:23 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='font-bold bg-white text-lg h-screen w-full '>
+dddnd n l,d,nedjndj
+    </div>
+      
+  )
+}
+
+export default App
